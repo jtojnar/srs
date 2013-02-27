b14be878cff3faebe98d05bf65b5e483bb78ea55
Jan Tojnar <jtojnar@gmail.com>
2013-02-27 23:15:27 +0100

    Added my decks page (generated from JSON for now) also unstyled

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get('/', function(req, res){
 	res.render('index', {title: 'miwi'});
 });
 
+app.get('/decks', function(req, res){
+	res.render('decks', {title: 'My decks', decks: [{id: 1, name: 'English', slug: 'english', user_id: 1, description: 'Basic english words', stats: {new: 20, review: 15, again: 3, total: 2600}}, {id: 1, name: 'Heisigs Remember the Kanji (RTK)', slug: 'rtk1', user_id: 1, description: 'Classic book for learning kanji', stats: {new: 2, review: 1, again: 0, total: 2520}}]});
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Started app on port %d', app.get('port'));
 });
